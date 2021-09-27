edda6e9d37289d4b7ed9150127e8107d636efbe3
Ruslan Kuyanets <ruslan.kuyanets@yandex.ru>
2021-09-27 15:58:17 +0300

    test api 2

--- src/js/economicNews.js
+++ src/js/economicNews.js
@@ -1,17 +1,22 @@
-const API_KEY_NEWS = '1de49cf49c8b420c9333bade896baea7'
+const api_key_test = 'EbXM4yKB9ua_wBL-6CihPQbg2YAZyOrKRYPxMty9A1U'
 
 const getNews = (category) => {
-    fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY_NEWS}`)
+    fetch(`https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=${category}`, {
+        headers: {
+            'x-api-key': api_key_test
+        }
+    })
     .then(resp => resp.json())
     .then(data => {
         const dataNews = data.articles
         for (let i = 0; i < 7; i++) {
-            if (dataNews[i].urlToImage == null) {
-                dataNews[i].urlToImage = 'img/category/worldNews.png'
+            if (dataNews[i].media == null) {
+                dataNews[i].media = 'img/category/worldNews.png'
             }
-            if (dataNews[i].description == 'False') {
-                i++
-            }
+            // if (dataNews[i].description == 'False') {
+            //     i++
+            // }
+            // console.log(dataNews[i].media)
             createCardsNews(dataNews[i], category)
         }      
     })
@@ -27,23 +32,23 @@ const createCardsNews = (data, category) => {
 
     const cardInfo = createElem('div', 'news-card__info');
     card.append(cardInfo)
-    const cardInfoName = createElem('h3', 'news-card__info-name', data.source.name);
+    const cardInfoName = createElem('h3', 'news-card__info-name', data.author); //data.source.name
     cardInfo.append(cardInfoName)
     const cardInfoCategory = createElem('h4', 'news-card__info-category', category);
     cardInfo.append(cardInfoCategory)
 
-    const cardTitle = createElem('p', 'news-card__title', data.title);
+    const cardTitle = createElem('p', 'news-card__title', data.title); //data.title
     card.append(cardTitle)
 
-    const cardDescription = createElem('p', 'news-card__description', data.description);
+    const cardDescription = createElem('p', 'news-card__description', data.excerpt); //data.description
     card.append(cardDescription)
 
     const cardImg = createElem('img', 'news-card__img');
-    cardImg.src = data.urlToImage
+    cardImg.src = data.media   //data.urlToImage
     card.append(cardImg)
 
     const cardURL = createElem('a', 'news-card__url', 'Click to continue');
-    cardURL.href = data.url
+    cardURL.href = data.link  //data.url
     card.append(cardURL)
 }
 
@@ -73,12 +78,22 @@ const chooseCategory = () => {
 }
 chooseCategory()
 
-const api_key_test = 'EbXM4yKB9ua_wBL-6CihPQbg2YAZyOrKRYPxMty9A1U'
-const url_test = 'https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=business'
-fetch(url_test, {
-    headers: {
-        'x-api-key': api_key_test
-    }
-}).then(resp => resp.json()).then(data => {
-    console.log(data.articles)
-})
+
+// const getNews = (category) => {
+//     fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY_NEWS}`)
+//     .then(resp => resp.json())
+//     .then(data => {
+//         const dataNews = data.articles
+//         for (let i = 0; i < 7; i++) {
+//             if (dataNews[i].urlToImage == null) {
+//                 dataNews[i].urlToImage = 'img/category/worldNews.png'
+//             }
+//             if (dataNews[i].description == 'False') {
+//                 i++
+//             }
+//             createCardsNews(dataNews[i], category)
+//         }      
+//     })
+// }
+//const API_KEY_NEWS = '1de49cf49c8b420c9333bade896baea7'
+
